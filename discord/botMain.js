3bc25f8af88f53c895247f03a17ec187eb80dda1
Overflow2147 <41734674+Overflow2147@users.noreply.github.com>
2018-07-26 16:56:19 +0500

    Keeping to update the 1.0.0

--- discord/botMain.js
+++ discord/botMain.js
@@ -14,7 +14,18 @@ client.on("message", message => {
    let args = messageArray.slice(1);
 
    if(cmd === "ov>yeet") {
-     message.channel.send(":ok_hand: :joy: :ok_hand:");
+     message.channel.send(":ok_hand: :joy: :ok_hand: :100:");
+   }
+   if(cmd === "ov>help") {
+     message.channel.send("List of modules: \n \n :one:   Basic \n :two:   Memes \n \n Type ov>mhelp [module name] to get a list of commands in a specific module.")
+   }
+   if(cmd === "ov>mhelp") {
+     if(args[0] === "Basic") {
+       message.channel.send("List of commands in Basic module: \n \n ov>help - Display the list of modules. \n ov>mhelp [module name] - Display the list of commands in a specific module.")
+     }
+     if(args[0] === "Memes") {
+       message.channel.send("List of commands in Memes module: \n \n ov>yeet - Makes the bot to what")
+     }
    }
 })
 client.login("token");
