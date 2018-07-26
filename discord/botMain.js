85d93f72abaaf8e0f250ca2159353506239dd112
Overflow2147 <41734674+Overflow2147@users.noreply.github.com>
2018-07-26 16:29:45 +0500

    Updated the source code to 1.0.0

--- discord/botMain.js
+++ discord/botMain.js
@@ -2,6 +2,19 @@ const Discord = require("discord.js");
 const client = new Discord.Client({disableEveryone: true})
 
 client.on("ready", () => {
-  console.log("Ready to run!")
+  console.log("Ready to run!");
+  client.user.setGame("Minecrap")
+})
+client.on("message", message => {
+   if(message.author.bot) return;
+   if(message.channel.type === "dm") return;
+
+   let messageArray = message.content.split(" ")
+   let cmd = messageArray[0]
+   let args = messageArray.slice(1);
+
+   if(cmd === "ov>yeet") {
+     message.channel.send(":ok_hand: :joy: :ok_hand:");
+   }
 })
 client.login("token");
