c55858496a6c9a775e6f0e02ad07831d02d20296
Overflow2147 <41734674+Overflow2147@users.noreply.github.com>
2018-07-26 16:11:26 +0500

    Update the main bot file
    Removed the conf constant with the botConf.json file

--- discord/botMain.js
+++ discord/botMain.js
@@ -1,8 +1,7 @@
-const conf = require("./botConf.json");
 const Discord = require("discord.js");
 const client = new Discord.Client({disableEveryone: true})
 
 client.on("ready", () => {
   console.log("Ready to run!")
 })
-client.login(conf.token);
+client.login("token");
